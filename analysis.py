3fe789cdab988077c0dad66a42c36c46b0eddcfe
danielgarza <danielgarza22@gmail.com>
2021-11-05 17:51:57 -0400

    improved analysis

--- analysis.py
+++ analysis.py
@@ -11,42 +11,45 @@ winers = {}
 
 for row in read_csv:
 
-    total += 1
     party = row['party']
     state = row['state']
     county = row['county']
     candidate = row['candidate']
     win = row['won']
 
-    if state not in states.keys():
-        states[state] = 0
-
-    if county not in counties.keys():
-        counties[county] = 0
-
-    if party not in parties.keys():
-        parties[party] = 0
-
     if candidate not in candidates.keys():
-        candidates[candidate] = 0
+        candidates[candidate] = {}
 
-    if win not in winers.keys():
-        winers[win] = 0
+    # if state not in states.keys():
+    candidates[candidate]['party'] = {
+        party: {
+            'state':{
+                state:{
+                    'county':{
+                        county:{
+                            'votes': row['total_votes'],
+                            'won':row['won']
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-    states[state] = 0
-    parties[party] = 0
-    counties[county] = 0
-    candidates[candidate] = 0
 
 output = f'''
 =====================PRESIDENTIAL=ANALYSIS=2020====================
-\nTotal rows: {total}
 Total candidates: {len(candidates)}
-Total states: {len(states)}
-Total counties: {len(counties)}
-Total parties: {len(parties)}
+candidates: 
+===================================================================
+{[ cand  for cand in candidates.keys() ]}
+===================================================================
+
 
 '''
+# Total states: {len(states)}
+# Total counties: {len(counties)}
+# Total parties: {len(parties)}
 
 
 
