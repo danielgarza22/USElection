cbe73aae6fa1f03f756600f561f3291fd76be21c
danielgarza <danielgarza22@gmail.com>
2021-11-11 17:58:23 -0500

    modified script

--- analysis.py
+++ analysis.py
@@ -15,26 +15,28 @@ for row in read_csv:
     state = row['state']
     county = row['county']
     candidate = row['candidate']
+    votes = row['total_votes']
     win = row['won']
+    
 
     if candidate not in candidates.keys():
         candidates[candidate] = {}
 
     # if state not in states.keys():
-    candidates[candidate]['party'] = {
-        party: {
-            'state':{
-                state:{
-                    'county':{
-                        county:{
-                            'votes': row['total_votes'],
-                            'won':row['won']
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    candidates[candidate]['party'] = party
+
+    if 'state' not in candidates[candidate]:
+        candidates[candidate]['state'] = {}
+    
+    if state not in candidates[candidate]['state']:
+        candidates[candidate]['state'][state] = {}
+
+    if county not in  candidates[candidate]['state'][state]:
+         candidates[candidate]['state'][state][county] = {}
+
+    candidates[candidate]['state'][state][county]['votes'] = votes    
+    candidates[candidate]['state'][state][county]['won'] = win    
+
 
 
 output = f'''
@@ -45,11 +47,18 @@ candidates:
 {[ cand  for cand in candidates.keys() ]}
 ===================================================================
 
-
 '''
-# Total states: {len(states)}
-# Total counties: {len(counties)}
-# Total parties: {len(parties)}
+for cand in candidates.keys():
+    output += cand
+    output += '\nParty: ' + candidates[cand]['party'] + '\n'
+
+    for loc in candidates[cand]['state']:
+        output += loc
+        output += '\n===================================================================\n'
+
+
+
+output += '\n===================================================================\n'
 
 
 
